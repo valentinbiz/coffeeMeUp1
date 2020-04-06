6293d051d614fc5a50357c276ffb18a7a15c67a8
Valentin Bizgan <valentin.bizzz@gmail.com>
2020-04-06 16:35:53 +0100

    Updates

---
+++ precache-manifest.3df719149959c361d0995baab2e142ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a51c5904035d5c8a4356bfe6dc884bf",
+    "url": "/coffeeMeUp1/index.html"
+  },
+  {
+    "revision": "3ad6a3eb1dc10f1ef872",
+    "url": "/coffeeMeUp1/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "b71d1da80295d7cb9239",
+    "url": "/coffeeMeUp1/static/css/main.8f179541.chunk.css"
+  },
+  {
+    "revision": "3ad6a3eb1dc10f1ef872",
+    "url": "/coffeeMeUp1/static/js/2.8c26e55d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coffeeMeUp1/static/js/2.8c26e55d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b71d1da80295d7cb9239",
+    "url": "/coffeeMeUp1/static/js/main.41fbde38.chunk.js"
+  },
+  {
+    "revision": "f16bdf312729add54024",
+    "url": "/coffeeMeUp1/static/js/runtime-main.5c793d65.js"
+  },
+  {
+    "revision": "ff59d261efe687ce670ce4d035a39c84",
+    "url": "/coffeeMeUp1/static/media/SimpleHandwriting-Regular.ff59d261.woff"
+  }
+]);
